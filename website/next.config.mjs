91b2985ed09c9575a5369fdd27459289fdcd0c93
ogawa <ogawa@ab-net.co.jp>
2025-01-09 14:08:12 +0900

    初回コミット ・設定ファイルの修正

--- website/next.config.mjs
+++ website/next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isProd = process.env.NODE_ENV === 'production';
+const nextConfig = {
+    reactStrictMode: true,
+    trailingSlash: true,
+    poweredByHeader: false,
+    compiler: {
+        removeConsole: isProd,
+        styledComponents: true,
+    },
+};
 
 export default nextConfig;
