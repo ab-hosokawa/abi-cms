e0d03127ee72e563cdcd1fc5e2e87416cd2be45d
GotoSeigo <goto.seigo.abnet@gmail.com>
2025-02-05 18:13:56 +0900

    マイグレーションファイル　コミット

--- backend/src/app/Http/Controllers/Api/Step1_2/FrontendController.php
+++ backend/src/app/Http/Controllers/Api/Step1_2/FrontendController.php
@@ -3,21 +3,54 @@
 namespace app\Http\Controllers\Api\Step1_2;
 
 use App\Http\Controllers\Controller;
+use App\Models\Article;
 use Illuminate\Http\Request;
 
 class FrontendController extends Controller
 {
     public function index(Request $request)
     {
-        $data = [
-            'status_code' => 200,
-            'contents' => [
-                'id' => 1,
-                'title' => 'abi-cms',
-                'body' => 'abi-cmsのSTEP1-1'
-            ]
-        ];
+        $posts = Article::all();
 
-        return response()->json($data);
+        return response()->json([
+            'success' => true,
+            'timestamp' => now()->timestamp,
+            'payload' => [
+                'data' => $posts,
+            ]
+        ],200);
+    }
+
+    public function store(Request $request)
+    {
+        $posts = Article::all();
+
+        return response()->json([
+            'success' => true,
+            'timestamp' => now()->timestamp,
+            'payload' => [
+                'data' => $posts,
+            ]
+        ],200);
+    }
+
+    public function show(Request $request, $id)
+    {
+        $post = Article::find($id);
+
+        if (!$post) {
+            return response()->json([
+                'message' => 'Post not found',
+                'sent_at' => now()->timestamp,
+            ], 404);
+        }
+
+        return response()->json([
+            'success' => true,
+            'timestamp' => now()->timestamp,
+            'payload' => [
+                'data' => $post,
+            ]
+        ],200);
     }
 }
--- backend/src/app/Http/Controllers/Api/Step1_2/WebsiteController.php
+++ backend/src/app/Http/Controllers/Api/Step1_2/WebsiteController.php
@@ -9,15 +9,7 @@ class WebsiteController extends Controller
 {
     public function index(Request $request)
     {
-        $data = [
-            'status_code' => 200,
-            'contents' => [
-                'id' => 1,
-                'title' => 'abi-cms',
-                'body' => 'abi-cmsのSTEP1-1'
-            ]
-        ];
 
-        return response()->json($data);
+        return response()->json();
     }
 }
