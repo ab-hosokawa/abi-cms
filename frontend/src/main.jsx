8b52ff72362c71769e06b263684362d6e08cd8b1
tamada <tamada@ab-net.co.jp>
2025-02-20 18:35:12 +0900

    レイアウト調整

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,19 +3,43 @@ import { createRoot } from 'react-dom/client'
 import { HashRouter, Route, Routes } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
+import { Col, Container, Nav, Navbar, Row } from 'react-bootstrap'
+import { Step1_2 } from './pages/Step1_2.jsx'
 
-const routes = [{ path: '/', element: App }]
+const routes = [
+  { path: '/', element: App, name: 'Top' },
+  { path: '/step1_2/', element: Step1_2, name: 'Step1-2' },
+]
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter>
-      <Routes>
-        {routes.map((route, key) => {
-          console.log(route)
-          return <Route path={route.path} element={<route.element />} key={key} />
-        })}
-        <Route path={'/'} element={<App />} />
-      </Routes>
-    </HashRouter>
+    <Navbar bg={'dark'} variant={'dark'} expand={'lg'}>
+      <Navbar.Brand>abi-CMS</Navbar.Brand>
+    </Navbar>
+    <Container fluid>
+      <Row>
+        <Col xs={2} className={'bg-light vh-100'}>
+          <Nav className={'flex-column p-3'}>
+            {routes.map((route, key) => {
+              return (
+                <Nav.Link href={'/#' + route.path} key={key}>
+                  {route.name}
+                </Nav.Link>
+              )
+            })}
+          </Nav>
+        </Col>
+        <Col xs={10} className="p-4">
+          <HashRouter>
+            <Routes>
+              {routes.map((route, key) => {
+                return <Route path={route.path} element={<route.element />} key={key} />
+              })}
+              <Route path={'/'} element={<App />} />
+            </Routes>
+          </HashRouter>
+        </Col>
+      </Row>
+    </Container>
   </StrictMode>
 )
