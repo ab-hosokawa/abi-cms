6d83721a28ad98d411fae5a976310e68f681448e
k-ayaka-0606 <kukita.ayaka.abnet@gmail.com>
2025-02-25 20:00:25 +0900

    STEP1-3 Web site側api作成

--- backend/src/app/Http/Controllers/Api/Step1_3/WebsiteController.php
+++ backend/src/app/Http/Controllers/Api/Step1_3/WebsiteController.php
@@ -10,12 +10,19 @@ class WebsiteController extends Controller
 {
     public function index(Request $request)
     {
-        $posts = Article::all();
-
+        $limit = $request->input('limit', 10);
+        $current = $request->input('current', 1);
+    
+        $posts = Article::paginate($limit, ['*'], 'page', $current);
+    
         return response()->json([
             'success' => true,
             'timestamp' => now()->timestamp,
-            'contents' => $posts
-        ],200);
+            'total' => $posts->total(),         // 全件数
+            'current' => $posts->currentPage(), // 表示ページ番号
+            'pages' => $posts->lastPage(),   // 総ページ数
+            'limit' => $posts->perPage(),     // 1ページの表示件数
+            'contents' => $posts->items(),
+        ]);
     }
 }
