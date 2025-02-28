52d98b959bb2a29187250efca0d5dfd0dc3cfe5c
ogawa <ogawa@ab-net.co.jp>
2025-02-28 18:35:34 +0900

    ・サイトが見れない問題の調整中

--- website/next.config.mjs
+++ website/next.config.mjs
@@ -1,57 +1,12 @@
-import CompressionPlugin from 'compression-webpack-plugin'; // 圧縮用プラグイン
 /** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
-    reactStrictMode: !!isProd,
+    reactStrictMode: true,
     trailingSlash: true,
-    // 静的ファイルの配信設定
-    compress: true,
     poweredByHeader: false,
     compiler: {
         removeConsole: isProd,
         styledComponents: true,
-    },
-    webpack(config, { isServer }) {
-        // if (!isServer) {
-        //     config.plugins.push(localesPlugin.webpack({ locales: [] }));
-        // }
-        config.module.rules.push({
-            test: /\.svg$/,
-            use: ({ resource }) => ({
-                loader: '@svgr/webpack',
-                options: {
-                    svgoConfig: {
-                        plugins: [
-                            {
-                                name: 'preset-default',
-                                params: {
-                                    overrides: {
-                                        removeViewBox: false,
-                                        cleanupIds: false,
-                                    },
-                                },
-                            },
-                        ],
-                    },
-                },
-            }),
-        });
-        config.watchOptions = {
-            poll: 1000,
-            aggregateTimeout: 300,
-            ignored: /node_modules/
-        };
-        /**
-         * プロダクション環境のみ、ソース圧縮と余分なCSSを削除する
-         */
-        if (isProd) {
-            config.plugins.push(
-                new CompressionPlugin({
-                    algorithm: 'brotliCompress',
-                })
-            );
-        }
-        return config;
     },
 };
 
