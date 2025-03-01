34795568428bb585bd3a1b52b7dcba48cfdad989
細川健一 <>
2025-03-01 10:51:28 +0900

    調整

--- website/next.config.mjs
+++ website/next.config.mjs
@@ -1,11 +1,12 @@
-import CompressionPlugin from 'compression-webpack-plugin'; // 圧縮用プラグイン
+// next.config.mjs
+
+const CompressionPlugin = require('compression-webpack-plugin');  // CommonJS形式
 
 /** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
     reactStrictMode: !!isProd,
     trailingSlash: true,
-    // 静的ファイルの配信設定
     compress: true,
     poweredByHeader: false,
     compiler: {
@@ -13,16 +14,6 @@ const nextConfig = {
         styledComponents: true,
     },
     webpack(config, { isServer }) {
-        if (isProd) {
-            config.plugins.push(
-                new CompressionPlugin({
-                    test: /\.js$|\.css$|\.html$/, // 圧縮対象のファイル
-                    threshold: 8192, // 8KB 以上のファイルを圧縮
-                    minRatio: 0.8, // 圧縮率が0.8未満のファイルを圧縮
-                })
-            );
-        }
-
         config.module.rules.push({
             test: /\.svg$/,
             use: ({ resource }) => ({
@@ -44,15 +35,13 @@ const nextConfig = {
                 },
             }),
         });
-
         config.watchOptions = {
             poll: 1000,
             aggregateTimeout: 300,
             ignored: /node_modules/,
         };
-
         return config;
     },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
