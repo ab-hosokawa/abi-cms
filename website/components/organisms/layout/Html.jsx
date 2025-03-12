eb575fd00d6e43004b111189b315d98a0954fa0c
ogawa <ogawa@ab-net.co.jp>
2025-03-12 17:29:17 +0900

    ・websiteコンテナ内のnpm installし直す

--- website/components/organisms/layout/Html.jsx
+++ website/components/organisms/layout/Html.jsx
@@ -1,23 +1,14 @@
 'use client';
 import Header from '@/components/organisms/layout/Header';
 import Footer from '@/components/organisms/layout/footer';
-import { Geist, Geist_Mono } from 'next/font/google';
 import { StoreProvider } from '@/lib/context/StoreContext';
 import { ThemeProvider } from '@/lib/context/ThemeContext';
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
 export default function Html({ data, child }) {
   return (
     <html lang="ja">
       <StoreProvider data={data}>
-        <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <body>
           <ThemeProvider>
             <Header />
             <main className="container mx-auto my-7">{child}</main>
