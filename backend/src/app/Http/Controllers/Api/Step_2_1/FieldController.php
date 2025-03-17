9d737df71206196f90dfc2b5c42e1bcfe5a63f0a
後藤盛悟 <  >
2025-03-17 20:32:29 +0900

    Step2_1 backend 下準備

---
+++ backend/src/app/Http/Controllers/Api/Step_2_1/FieldController.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace app\Http\Controllers\Api\Step_2_1;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\Request;
+
+class FieldController extends Controller
+{
+    /** 一覧
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function index(Request $request)
+    {
+        $limit = $request->input('limit', 10);
+        $current = $request->input('current', 1);
+
+        $posts = Article::paginate($limit, ['*'], 'page', $current);
+
+        return response()->json([
+            'success' => true,
+            'timestamp' => now()->timestamp,
+            'payload' => [
+                'total' => $posts->total(),
+                'current' => $posts->currentPage(),
+                'pages' => $posts->lastPage(),
+                'data' => $posts->items(),
+            ]
+        ],200);
+    }
+
+    /** 編集
+     * @param Request $request
+     * @param $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function edit(Request $request, $id)
+    {
+        try {
+            $post = Article::findOrFail($id);
+
+            return response()->json([
+                'success' => true,
+                'timestamp' => now()->timestamp,
+                'payload' => [
+                    'data' => $post,
+                ]
+            ],200);
+
+        } catch(ModelNotFoundException $e) {
+            return response()->json([
+                'message' => $e->getMessage(),
+            ],404);
+
+        } catch(\Exception $e) {
+            return response()->json([
+                'message' => $e->getMessage(),
+            ],500);
+
+        }
+    }
+
+    /** 更新
+     * @param Request $request
+     * @param $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function update(Request $request, $id)
+    {
+        try {
+            $validatedData = $request->validate([
+                'title' => 'required|string',
+                'body' => 'required|string',
+            ]);
+
+            $post = Article::findOrFail($id);
+
+            $post->update($validatedData);
+
+            return response()->json([
+                'success' => true,
+                'timestamp' => now()->timestamp,
+                'payload' => [
+                    'data' => $post,
+                ]
+            ], 204);
+
+        } catch(ModelNotFoundException $e) {
+            return response()->json([
+                'message' => $e->getMessage(),
+            ],404);
+
+        } catch(\Exception $e) {
+            return response()->json([
+                'message' => $e->getMessage(),
+            ],500);
+
+        }
+    }
+
+    /** 削除
+     * @param Request $request
+     * @param $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function destroy(Request $request, $id)
+    {
+        try {
+            $post = Article::findOrFail($id);
+
+            $post->delete();
+
+            return response()->json([
+                'success' => true,
+                'timestamp' => now()->timestamp,
+                'payload' => [
+                    'id' => $post->id,
+                    'result' => true,
+                ]
+            ], 204);
+        } catch(ModelNotFoundException $e) {
+            return response()->json([
+                'message' => $e->getMessage(),
+            ],404);
+
+        } catch(\Exception $e) {
+            return response()->json([
+                'message' => $e->getMessage(),
+            ],500);
+
+        }
+    }
+
+}
---
+++ backend/src/app/Models/Step2_1/Model.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\Step2_1;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Model extends Model
+{
+    //
+}
