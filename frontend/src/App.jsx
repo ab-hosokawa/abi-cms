2b9055b9a451673df59d09a98be44aeee99d5a1d
tamada <tamada@ab-net.co.jp>
2025-04-14 09:23:05 +0900

    環境変数確認2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import { Heading } from './utils/components/parts/Heading.jsx'
 
 function App() {
-  console.log(import.meta.env)
+  console.log(import.meta.env.MODE)
   return (
     <>
       <Heading title={'Dashboard'} />
